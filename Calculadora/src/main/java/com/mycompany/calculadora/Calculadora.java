a5bfb0e54921fbcc069ac4bce34571a34495d477
Dvo <dvidorr165@g.educaand.es>
2024-02-19 16:56:55 +0100

    Creo el proyecto

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public int suma(int numero1, int numero2){
+        int suma;
+        
+        suma = numero1 + numero2; 
+        
+        return suma;
+    }
+}
